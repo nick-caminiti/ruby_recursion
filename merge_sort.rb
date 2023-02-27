612fc8fdf3445f5ed5db3af4f18bc0c0b4bb0f19
Nick Caminiti <nmcams11@gmail.com>
2023-02-27 10:54:04 -0800

    merge sort working

--- merge_sort.rb
+++ merge_sort.rb
@@ -5,11 +5,22 @@ def merge_sort(array, sorted_array = [])
     array
   else
     left_array = merge_sort(array[0, array.length / 2])
-    right_array = merge_sort(array[array.length / 2, array.length])
+    right_array = merge_sort(array[array.length / 2, array.length - 1])
     # merge into sorted array
+    while left_array.length.positive? && right_array.length.positive?
+      if left_array[0] < right_array[0]
+        sorted_array << left_array[0]
+        left_array.delete_at(0)
+      else
+        sorted_array << right_array[0]
+        right_array.delete_at(0)
+      end
+    end
+    (sorted_array << right_array).flatten! if left_array.empty?
+    (sorted_array << left_array).flatten! if right_array.empty?
     sorted_array
   end
 end
 
-array = [2, 3, 9, 7, 2, 5, 1, 6]
-p merge_sort(array, sorted_array)
+array = [2, 3, 99, 99, 1000, 9, -1, 7, 0, 45, 2, 5, 1, 6]
+p merge_sort(array)
